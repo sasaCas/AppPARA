8ba47550b6cdf31ac748873c50a4a3bb4eea5d22
mac <x>
2017-09-21 21:18:47 +0200

    Layout - FINALIZADO BASE de las dos primeras pantallas. TODO la última.

--- app/src/main/java/com/example/mac/apppara/MainActivity.java
+++ app/src/main/java/com/example/mac/apppara/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mac.apppara;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void numeroGanador(View view) {
+
+        Intent intentQuePasaDeActividadDeGuay = new Intent(this, Activity_1.class);
+        startActivity(intentQuePasaDeActividadDeGuay);
+    }
+
+
+    public void eresUnPocoTonto(View view) {
+        Toast.makeText(this, "Eres un poco tonto, POR FAVORRRR", Toast.LENGTH_SHORT).show();
     }
 }
