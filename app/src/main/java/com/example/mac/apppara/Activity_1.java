ae1b3a88c270f492c3ba9a6087f8f2043c44c190
mac <x>
2017-09-22 17:33:38 +0200

    Layout - onClick s terminados

--- app/src/main/java/com/example/mac/apppara/Activity_1.java
+++ app/src/main/java/com/example/mac/apppara/Activity_1.java
@@ -2,6 +2,8 @@ package com.example.mac.apppara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Activity_1 extends AppCompatActivity {
 
@@ -10,4 +12,22 @@ public class Activity_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
     }
+
+    public void eres88 (View view) {
+        Toast.makeText(this, "¿no se si eres muy listo?", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    public void eres72 (View view) {
+        Toast.makeText(this, "creo que no llegaras al final", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    public void eres86 (View view) {
+        Toast.makeText(this, "ya no se que que decir, de lo mal que lo haces", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
--- app/src/main/java/com/example/mac/apppara/MainActivity.java
+++ app/src/main/java/com/example/mac/apppara/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,4 +28,20 @@ public class MainActivity extends AppCompatActivity {
     public void eresUnPocoTonto(View view) {
         Toast.makeText(this, "Eres un poco tonto, POR FAVORRRR", Toast.LENGTH_SHORT).show();
     }
+
+
+    public void eres32(View view) {
+        Toast.makeText(this, "no eres muy listo, por favor", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+        public void eres68(View view) {
+            Toast.makeText(this, "no pareces muy listo, por favor", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
 }
