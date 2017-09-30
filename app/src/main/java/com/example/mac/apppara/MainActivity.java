de1984e61c2fe830abc59f72ea4f757d68c3df27
mac <x>
2017-09-30 17:51:34 +0200

    FINAL COMIT

--- app/src/main/java/com/example/mac/apppara/MainActivity.java
+++ app/src/main/java/com/example/mac/apppara/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+
+// APLICACIÓN HECHA POR PAPÁ Y SARA
+
 public class MainActivity extends AppCompatActivity {
 
 
